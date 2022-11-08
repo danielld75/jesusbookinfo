a0b7d6080628d22b3cee84f682aa444bc7657ca0
danielld75 <danielld@poczta.fm>
2022-11-08 16:53:38 +0100

    remove from Appjs dark sites

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import Home from "./pages/home/Home";
-import Login from './pages/login/Login';
-import Register from "./pages/register/Register";
-const DAY = "Monday"
 
 function App() {
   return (
     <div className="App">
       <Home />
-      {/*<Login />*/}
-      {/*<Register />*/}
     </div>
   );
 }
